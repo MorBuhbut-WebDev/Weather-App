4a48329320d98c5376d898102fdf0bbee86cdab7
Mor Buhbut <mbuhhbut2005@gmail.com>
2025-06-21 17:55:24 -0700

    Fixed the toggle location style

--- src/Components/Header/ToggleLoc/ToggleLoc.jsx
+++ src/Components/Header/ToggleLoc/ToggleLoc.jsx
@@ -2,11 +2,22 @@ import styles from "./ToggleLoc.module.css";
 
 export default function ToggleLoc() {
   return (
-    <div className={styles.buttonsWrapper}>
-      <button className={`${styles.toggleBtn} ${styles.activeBtn}`}>
-        Approve Loc
-      </button>
-      <button className={styles.toggleBtn}>Decline Loc</button>
+    <div className={styles.locationWrapper}>
+      <p className={styles.labelMessage}>📍 Use My Location?</p>
+      <div className={styles.buttonsWrapper}>
+        <button
+          className={`${styles.toggleBtn} ${styles.activeBtn}`}
+          aria-label="Approve Location Access"
+        >
+          Yes
+        </button>
+        <button
+          className={styles.toggleBtn}
+          aria-label="Decline Location Access"
+        >
+          No
+        </button>
+      </div>
     </div>
   );
 }
