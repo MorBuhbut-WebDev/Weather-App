d23822a2afdd07199e963f25387e0a018e30ece1
Mor Buhbut <mbuhhbut2005@gmail.com>
2025-06-22 04:15:53 -0700

    Added Api Layer

---
+++ src/Api/weatherApi.js
@@ -0,0 +1,25 @@
+const apiKey = import.meta.env.VITE_API_KEY;
+
+async function fetchData(url) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`Http Error With Status Code: ${res.status}`);
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export async function fetchGeoLocation(city) {
+  const baseGeoUrl = import.meta.env.VITE_BASE_GEO_URL;
+  const url = `${baseGeoUrl}?q=${city}&limit=5&appid=${apiKey}`;
+  return await fetchData(url);
+}
+
+export async function fetchWeeklyForecast(lat, lon) {
+  const baseWeatherUrl = import.meta.env.VITE_BASE_WEATHER_URL;
+  const url = `${baseWeatherUrl}?lat=${lat}&lon=${lon}&cnt=8&units=metric&lang=en&appid=${apiKey}`;
+  return await fetchData(url);
+}
