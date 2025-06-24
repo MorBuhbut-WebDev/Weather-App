3230dfe69332af942759ad474a90541b8316ecf2
Mor Buhbut <mbuhhbut2005@gmail.com>
2025-06-23 20:50:34 -0700

    Added Suggestion List (Only Styling)

--- src/Components/Header/SearchBar/SearchBar.jsx
+++ src/Components/Header/SearchBar/SearchBar.jsx
@@ -1,13 +1,28 @@
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar() {
+  const mockUpList = [
+    { label: "Paris, Île-de-France, FR", lat: 48.8566, lon: 2.3522 },
+    { label: "Paris, Île-de-France, FR", lat: 48.8566, lon: 2.3522 },
+    { label: "Paris, Île-de-France, FR", lat: 48.8566, lon: 2.3522 },
+    { label: "Paris, Île-de-France, FR", lat: 48.8566, lon: 2.3522 },
+    { label: "Paris, Île-de-France, FR", lat: 48.8566, lon: 2.3522 },
+  ];
+
   return (
-    <form className={styles.form}>
+    <div className={styles.combobox}>
       <input
         className={styles.searchBar}
         type="text"
         placeholder="Search For Cities..."
       />
-    </form>
+      <ul className={styles.suggestionsList}>
+        {mockUpList.map((item) => (
+          <li key={item.label} className={styles.option} tabIndex={0}>
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
